82808f4efed03245d87d548e2cc74fbc6a8cf994
Ablazzing <y2286122@yandex.ru>
2022-07-12 23:29:04 +0300

    Лекция №2

--- src/lesson2/Lesson2.java
+++ src/lesson2/Lesson2.java
@@ -18,6 +18,14 @@ public class Lesson2 {
         //Повторить строку n раз
         String repeatedText = text.repeat(10);
 
+        //Добавление переноса строки
+        String newTextForMe = "Это текст" + '\n' + "Вторая часть";
+
+        //Создание строки из массива char
+        char[] letters = new char[]{'v','o','d','k','a'};
+        String charText = String.valueOf(letters);
+
+        //Примитивы (копируются по значению)
         int number = 10;
         int number2 = number;
 
@@ -25,7 +33,7 @@ public class Lesson2 {
         System.out.println(number);
         System.out.println(number2);
 
-        //
+        //Ссылочные типы данных (копируются по ссылке)
         int[] numbers = new int[]{1, 2, 3, 4};
         int[] numbers2 = numbers;
         numbers2[0] = 10;
@@ -33,15 +41,7 @@ public class Lesson2 {
         System.out.println(Arrays.toString(numbers));
         System.out.println(Arrays.toString(numbers2));
 
-        String[] strings = new String[]{"Строка 1", "Строка 2"};
-        System.out.println(Arrays.toString(strings));
 
-        String newTextForMe = "Это текст" + '\n' + "Вторая часть";
-        System.out.println(newTextForMe);
 
-        //Создание строки из массива char
-        char[] letters = new char[]{'v','o','d','k','a'};
-        String charText = String.valueOf(letters);
-        System.out.println(charText);
     }
 }
